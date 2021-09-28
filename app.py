38c86a59c744fa89afb064206e1f7786fbb16237
evelyne250 <uevelyne44@gmail.com>
2021-09-28 06:11:59 -0700

    dash

---
+++ app.py
@@ -0,0 +1,30 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_excel('polyfam.xlsx')
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    dcc.Dropdown(
+        id="ticker",
+        options=[{"label": x, "value": x} 
+                 for x in df.columns[1:]],
+        value=df.columns[1],
+        clearable=False,
+    ),
+    dcc.Graph(id="time-series-chart"),
+])
+
+@app.callback(
+    Output("time-series-chart", "figure"), 
+    [Input("ticker", "value")])
+def display_time_series(ticker):
+    fig = px.line(df, x='Date', y='Quantity')
+    return fig
+
+app.run_server(debug=True)
